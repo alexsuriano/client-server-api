638520eca9bdb1aa9b48cbd97f53c7193b945b2b
Alexandre Suriano <alexandre.suriano@gmail.com>
2023-09-28 16:32:35 -0300

    feat: server function to handle dollar exchange route

--- server/server.go
+++ server/server.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"context"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"time"
+)
+
+const (
+	urlDollarExchange = `https://economia.awesomeapi.com.br/json/last/USD-BRL`
+)
+
 type Dollar struct {
 	Usdbrl struct {
 		Code       string `json:"code"`
@@ -18,4 +31,52 @@ type Dollar struct {
 
 func main() {
 
+	http.HandleFunc("/cotacao", HandleDollarExchange)
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func GetDollarExchange(ctx context.Context, url string) (*Dollar, error) {
+
+	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var dollar Dollar
+
+	err = json.Unmarshal(body, &dollar)
+	if err != nil {
+		return nil, err
+	}
+
+	return &dollar, nil
+}
+
+func HandleDollarExchange(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithTimeout(context.Background(), 200*time.Millisecond)
+	defer cancel()
+
+	w.Header().Add("Content-Type", "application/json")
+
+	dollar, err := GetDollarExchange(ctx, urlDollarExchange)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		log.Println(err)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(dollar)
 }

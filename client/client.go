272c3cab8b0282869361f159cd2d34c83681700e
Alexandre Suriano <alexandre.suriano@gmail.com>
2023-10-02 22:39:04 -0300

    feat: add function to save file with dollar exchange

--- client/client.go
+++ client/client.go
@@ -7,11 +7,14 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 	"time"
 )
 
 const (
-	serverURL string = "http://127.0.0.1:8080/cotacao"
+	serverURL   string = "http://127.0.0.1:8080/cotacao"
+	cotacaoPath string = "cotacao.txt"
 )
 
 type ServerAPIResponse struct {
@@ -22,12 +25,20 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 300*time.Millisecond)
 	defer cancel()
 
-	dollar, err := getCotacao(ctx)
+	resp, err := getCotacao(ctx)
 	if err != nil {
 		log.Panic(err)
 	}
 
-	fmt.Println(dollar)
+	dollar, err := strconv.ParseFloat(resp.Bid, 64)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	err = saveDollarExchange(dollar)
+	if err != nil {
+		log.Panic(err)
+	}
 
 }
 
@@ -56,4 +67,31 @@ func getCotacao(ctx context.Context) (*ServerAPIResponse, error) {
 	}
 
 	return &dollar, err
+}
+
+func saveDollarExchange(dollar float64) error {
+
+	_, err := os.Stat(cotacaoPath)
+	if os.IsNotExist(err) {
+		file, err := os.Create(cotacaoPath)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+	}
+
+	file, err := os.OpenFile(cotacaoPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	data := fmt.Sprintf("Dólar: %v\n", dollar)
+
+	_, err = file.WriteString(data)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
